5057244049e5b2767e846a66864d81fac22b0fa9
Max Carvajal <jcarvajal288@gmail.com>
2022-06-01 07:39:23 -0500

    Added default api wrappings and starting routes

--- backend/project.clj
+++ backend/project.clj
@@ -4,7 +4,8 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
-                 [ring/ring-defaults "0.3.2"]]
+                 [ring/ring-defaults "0.3.2"]
+                 [ring/ring-json "0.5.1"]]
   :plugins [[lein-ring "0.12.5"]]
   :ring {:handler backend.handler/app}
   :profiles
--- backend/src/backend/handler.clj
+++ backend/src/backend/handler.clj
@@ -1,11 +1,19 @@
 (ns backend.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults secure-api-defaults]]
+            [ring.middleware.json :as json]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/api/matches" [] "TODO: return all matches")
+  (GET "/api/matches/:id" [] "TODO: return single match")
+  (POST "/api/matches" [] "TODO: create a match")
+  (PUT "/api/matches/:id" [] "TODO: update a match")
+  (DELETE "/api/matches/:id" [] "TODO: delete a match")
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (-> app-routes
+  (wrap-defaults api-defaults)
+  (json/wrap-json-params)
+  (json/wrap-json-response)))
