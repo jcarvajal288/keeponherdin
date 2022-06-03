a0cff3088f5a29ef9c7787dae37528e704080de8
Max Carvajal <jcarvajal288@gmail.com>
2022-06-02 23:30:31 -0500

    code cleanup

--- backend/project.clj
+++ backend/project.clj
@@ -2,19 +2,18 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [compojure "1.6.1"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [compojure "1.6.3"]
                  [com.layerware/hugsql-core "0.5.3"]
                  [com.layerware/hugsql-adapter-next-jdbc "0.5.3"]
                  [metosin/ring-http-response "0.9.3"]
                  [migratus "1.3.6"]
-                 [org.clojure/java.jdbc "0.7.12"]
                  [org.postgresql/postgresql "42.3.4"]
                  [org.slf4j/slf4j-log4j12 "2.0.0-alpha7"]
-                 [ring/ring-core "1.9.3"]
-                 [ring/ring-defaults "0.3.2"]
+                 [ring/ring-core "1.9.5"]
+                 [ring/ring-defaults "0.3.3"]
                  [ring/ring-json "0.5.1"]]
-  :plugins [[lein-ring "0.12.5"]
+  :plugins [[lein-ring "0.12.6"]
             [migratus-lein "0.7.3"]]
   :ring {:handler backend.handler/app}
   :migratus {:store :database
@@ -22,4 +21,4 @@
              :db {:connection-uri "jdbc:postgresql://localhost:5432/keeponherdin_dev?user=keeponherdin_dev&password=keeponherdin_dev"}}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.2"]]}})
+                        [ring/ring-mock "0.4.0"]]}})
--- backend/src/backend/matches.clj
+++ backend/src/backend/matches.clj
@@ -2,13 +2,9 @@
   (:require [backend.database :as db]
             [hugsql.core :as hugsql]
             [next.jdbc :as jdbc]
-            [hugsql.adapter.next-jdbc :as cj-adapter]
             ))
 
-(hugsql/def-db-fns "backend/matches.sql"
-                   {:adapter (cj-adapter/hugsql-adapter-next-jdbc)})
-
-(hugsql/def-sqlvec-fns "backend/matches.sql")
+(hugsql/def-sqlvec-fns "sql/matches.sql")
 
 (defn create-match [match]
   (let [conn (jdbc/get-datasource db/db-spec)]
